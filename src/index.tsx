2b765b12bf936375d5b839b36320ba1e220dd3f5
rodrigo <rodgomesc@gmail.com>
2021-08-24 22:10:16 -0300

    chore: create initial version for tests

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,33 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, ViewStyle, StyleProp } from 'react-native';
 
-export default (
-  <View>
-    <Text>Hello</Text>
+interface BlurViewProps {
+  blurType: 'xlight' | 'light' | 'dark' | 'extraDark' | 'regular' | 'prominent';
+  blurAmount: number;
+  reducedTransparencyFallbackColor?: string;
+  style?: StyleProp<ViewStyle>;
+  children: React.ReactElement;
+}
+
+const BlurView = ({
+  blurType,
+  blurAmount,
+  reducedTransparencyFallbackColor,
+  style,
+  children,
+  ...rest
+}: BlurViewProps): JSX.Element => (
+  <View
+    {...{
+      blurType,
+      blurAmount,
+      reducedTransparencyFallbackColor,
+      style: [style, { backdropFilter: `blur( 20.0px )` } as ViewStyle],
+      ...rest,
+    }}
+  >
+    {children}
   </View>
 );
+
+export default BlurView;
